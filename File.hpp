da0a0470cae67ee3220e10b320da7fd69176ef9e
CodeMouse179 <codemouse179@gmail.com>
2022-11-30 19:20:32 +0300

    Update File.hpp

--- File.hpp
+++ File.hpp
@@ -1,4 +1,18 @@
-﻿#ifndef SYSTEM_IO_FILE_HPP
+﻿//     +--------------------------------------------------------------------------------+
+//     |                                       File                                     |
+//     |  Introduction : System.IO.File in C++                                          |
+//     |  Modified Date : 2022/11/30                                                    |
+//     |  License : MIT                                                                 |
+//     |  Source Code : https://github.com/CodeMouse179/File                            |
+//     |  Readme : https://github.com/CodeMouse179/File/blob/main/README.md             |
+//     |                                                                                |
+//     |                             Designer : CodeMouse179                            |
+//     |  Email : codemouse179@gmail.com                                                |
+//     |  Github : https://github.com/CodeMouse179                                      |
+//     |  Bilibili : https://space.bilibili.com/3461577785215838                        |
+//     +--------------------------------------------------------------------------------+
+
+#ifndef SYSTEM_IO_FILE_HPP
 #define SYSTEM_IO_FILE_HPP
 
 namespace System
