031999f1a3e5dc5b401fcbbeb57db0e0aa25a4c9
CodeMouse179 <codemouse179@gmail.com>
2022-11-30 19:17:12 +0300

    Update File.hpp

--- File.hpp
+++ File.hpp
@@ -1,1 +1,21 @@
-﻿
+﻿#ifndef SYSTEM_IO_FILE_HPP
+#define SYSTEM_IO_FILE_HPP
+
+namespace System
+{
+    namespace IO
+    {
+        class File
+        {
+        private:
+            //static class
+            File()
+            {
+            }
+
+        public:
+        };
+    }
+}
+
+#endif
