ec595b43a3796262dd48eaa977b676e0afb2f9ce
CodeMouse179 <codemouse179@gmail.com>
2022-12-04 14:03:33 +0300

    add String.hpp, res/TestFile1, update File.hpp

--- Doc/README.md
+++ Doc/README.md
@@ -1,1 +1,3 @@
 # Documentation
+
+1. [How to check if directory exist using C++ and winAPI](https://stackoverflow.com/questions/8233842/how-to-check-if-directory-exist-using-c-and-winapi)
--- File.hpp
+++ File.hpp
@@ -1,7 +1,7 @@
 ﻿//     +--------------------------------------------------------------------------------+
 //     |                                       File                                     |
 //     |  Introduction : System.IO.File in C++                                          |
-//     |  Modified Date : 2022/12/3                                                     |
+//     |  Modified Date : 2022/12/4                                                     |
 //     |  License : MIT                                                                 |
 //     |  Source Code : https://github.com/CodeMouse179/File                            |
 //     |  Readme : https://github.com/CodeMouse179/File/blob/main/README.md             |
@@ -15,10 +15,27 @@
 #ifndef SYSTEM_IO_FILE_HPP
 #define SYSTEM_IO_FILE_HPP
 
+#include "String.hpp"
+
 namespace System
 {
     namespace IO
     {
+        struct FileAttributes
+        {
+        public:
+            bool Invalid;
+            bool IsDirectory;
+            bool IsArchive;
+
+            FileAttributes()
+            {
+                this->Invalid = false;
+                this->IsDirectory = false;
+                this->IsArchive = false;
+            }
+        };
+
         class File
         {
         private:
@@ -28,6 +45,71 @@ namespace System
             }
 
         public:
+            static bool Copy(const std::string& sourceFileName, const std::string& destFileName)
+            {
+                return false;
+            }
+
+            static bool Copy(const std::string& sourceFileName, const std::string& destFileName, bool overwrite)
+            {
+                return false;
+            }
+
+            static int Create(const std::string& path)
+            {
+                return 0;
+            }
+
+            static bool Delete(const std::string& path)
+            {
+                return false;
+            }
+
+            static bool Exists(const std::string& path)
+            {
+                FileAttributes fatt = File::GetAttributes(path);
+                if (fatt.Invalid) return false;
+                return fatt.IsArchive;
+            }
+
+            static FileAttributes GetAttributes(const std::string& path)
+            {
+                FileAttributes fatt;
+#ifdef SYSTEM_WINDOWS
+                std::wstring pathW;
+#ifdef SYSTEM_IO_FILE_ONLY_UTF8
+                pathW = StringA::StringToWstring(path, System::StringEncoding::UTF8);
+#else
+                if (StringA::IsValidUTF8(path))
+                {
+                    pathW = StringA::StringToWstring(path, System::StringEncoding::UTF8);
+                }
+                else
+                {
+                    pathW = StringA::StringToWstring(path, System::StringEncoding::ANSI);
+                }
+#endif
+                DWORD att = GetFileAttributesW(pathW.c_str());
+                if (att == INVALID_FILE_ATTRIBUTES)
+                {
+                    fatt.Invalid = true;
+                }
+                else
+                {
+                    if (att & FILE_ATTRIBUTE_DIRECTORY)
+                    {
+                        fatt.IsDirectory = true;
+                    }
+                    if (att & FILE_ATTRIBUTE_ARCHIVE)
+                    {
+                        fatt.IsArchive = true;
+                    }
+                }
+#endif
+#ifdef SYSTEM_LINUX
+#endif
+                return fatt;
+            }
         };
     }
 }
