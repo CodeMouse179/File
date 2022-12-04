2220fe656ed03c42fc4ede0732c38ff0966014fe
CodeMouse179 <codemouse179@gmail.com>
2022-12-04 23:17:07 +0300

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -5,8 +5,14 @@ using namespace System::IO;
 int main()
 {
     std::string workDirectory = File::CurrentDirectory();
+#ifdef SYSTEM_WINDOWS
     bool exists1 = File::Exists(workDirectory + U8("\\res\\TestFile1.txt"));
     bool exists2 = File::Exists(workDirectory + U8("\\res\\测试文件1.txt"));
+#endif
+#ifdef SYSTEM_LINUX
+    bool exists1 = File::Exists(workDirectory + U8("/res/TestFile1.txt"));
+    bool exists2 = File::Exists(workDirectory + U8("/res/测试文件1.txt"));
+#endif
     StringA::WriteLine(std::to_string(exists1));
     StringA::WriteLine(std::to_string(exists2));
     StringA::ReadLine();
